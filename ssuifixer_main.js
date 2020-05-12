7c0913691fb3cf43024bb069a8cc29b75d4dfd12
Joe Cory <hello@joecory.co.uk>
2020-05-12 10:53:19 +0100

    Changed to new AJAX listen methods

--- ssuifixer_main.js
+++ ssuifixer_main.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-const targetNode = window.parent.document;
-    var init = function(){
+  const targetNode = window.parent.document;
+  document.addEventListener('DOMContentLoaded', init);
+  window.addEventListener('mercury:load', init);
+
+  var init = function(){
+    if(ssuifixer == "Hker616"){
       $(targetNode).find('.sqs-custom-css-content .subtitle').html('');
       $(targetNode).find('.sqs-custom-css-content .CodeMirror-scroll').css('margin-bottom', '0px')
       $(targetNode).find('.sqs-custom-css-content .CodeMirror-scroll').css('padding-bottom', '0px')
@@ -10,26 +14,7 @@ const targetNode = window.parent.document;
           $(targetNode).find('.CodeMirror-sizer').css('margin-bottom', '12px')
         }
       })
-window.dispatchEvent(new Event('resize'));
+      window.dispatchEvent(new Event('resize'));
     }
-    if(ssuifixer == "Hker616"){
-    init()
-}
-    
-
-    const config = { attributes: true, childList: true, subtree: true };
-
-    const callback = function(mutationsList, observer) {
-      for(let mutation of mutationsList) {
-        if (mutation.attributeName === 'cm-not-content') {
-if(ssuifixer == "Hker616"){
-init()
-}
-        }
-      }
-    };
-
-    const observer = new MutationObserver(callback);
-
-    observer.observe(targetNode, config);
-  })
+  }
+})
